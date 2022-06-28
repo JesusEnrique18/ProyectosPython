5a825328cbdf52376c02d8f3adddc09248778fd6
JesúsEnrique18 <20193l001071@utcv.edu.mx>
2022-06-27 23:19:51 -0500

    cambios de codigo

--- Practicas_2/Ejercios Adicionales/Salario.py
+++ Practicas_2/Ejercios Adicionales/Salario.py
@@ -3,7 +3,7 @@ print("-------------------------------------------------------------------------
 sueldo = float (input ('Ingresa el  sueldo: '))
 if sueldo<1000:
     aumento=sueldo * 0.15 
+    sueldonuevo = sueldo + aumento
+    print (" Aumento $" + str (aumento) + " Nuevo sueldo $" + str (sueldonuevo))
 else:
-    aumento=0
-sueldonuevo = sueldo + aumento
-print (" Aumento $" + str (aumento) + " Nuevo sueldo $" + str (sueldonuevo))
+    print("No hay aumento")
--- Practicas_2/Ejercios Adicionales/tiempodeencuentro.py
+++ Practicas_2/Ejercios Adicionales/tiempodeencuentro.py
@@ -1,8 +1,10 @@
 
 Auto1 = float(input("Ingresa la velocidad en M/s del auto 1: "))
 Auto2 = float(input("Ingresa la velocidad en M/s del auto 2: "))
-Distanciainicial = float(input("Ingrese la distacia inicial M: "))
-Autos = Auto1 + Auto2
-tiempo = Distanciainicial/Autos
-
-print("Tiempo de encuentro entre los vehículos: "  + str(tiempo) +  " segundos")
+if Auto1 > 0 and Auto2 > 0:
+    Distanciainicial = float(input("Ingrese la distacia inicial M: "))
+    Autos = Auto1 + Auto2
+    tiempo = Distanciainicial/Autos
+    print("Tiempo de encuentro entre los vehículos: "  + str(tiempo) +  " segundos")
+else:
+    print("Datos proporcionados no validos")
